8cfae5c057c261c3c29ca986f1087eba2fe3c8d6
Haroon <haroon.ali.1104@gmail.com>
2023-08-30 13:45:21 +0100

    create test for getting videos

---
+++ server/tests/integration/videos.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const db = require('../../../client/cypress/db');
+const util = require('../util');
+
+const baseUrl = 'http://localhost:3001';
+let token;
+let userId = 1;
+
+describe('/api/videos', () => {
+    beforeAll(async () => {
+        token = await util.getToken();
+    });
+
+    beforeEach(async () => {
+        await db.seed();
+    });
+
+    afterAll(async () => {
+        db.pool.end();
+    });
+
+    it('should get videos', async () => {
+        const response = await request(baseUrl)
+            .get(`/api/videos/user/${userId}`)
+            .set('x-auth-token', token);
+
+        expect(response.body.videos[0]).toHaveProperty('video_id', 'NQ-2eJvakBo');
+    });
+});
